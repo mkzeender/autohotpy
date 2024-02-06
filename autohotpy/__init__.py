3edbdd654ddce570e361a0fa2976e85a9b744eeb
Marckie Zeender <mkzeender@gmail.com>
2024-02-05 22:35:04 -0800

    import into root module

--- autohotpy/__init__.py
+++ autohotpy/__init__.py
@@ -1,3 +1,6 @@
 from .ahk_instance import AhkInstance
 from .global_state import config
 from .ahk_run import ahk_runstr
+
+from .convenience.map_view import MapView
+from .convenience.py_lib import pylib as Python
